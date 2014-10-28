9b8ba60e116940176b9227d684a176e6b7872c5a
David Kudera <sakren@gmail.com>
2014-10-28 19:55:10 +0100

    removed usages of SplFileInfo::getExtensions() because of PHP 5.3.3

--- src/DK/ImagesManager/ImagesManager.php
+++ src/DK/ImagesManager/ImagesManager.php
@@ -345,7 +345,7 @@ class ImagesManager extends Object
 			$dir = pathinfo($path, PATHINFO_DIRNAME);
 
 			foreach (Finder::findFiles($shortName)->in($dir) as $image => $file) {		/** @var $file \SplFileInfo */
-				return $file->getExtension();
+				return pathinfo($image, PATHINFO_EXTENSION);		// because of PHP 5.3.3 https://travis-ci.org/sakren/nette-images-manager/jobs/39295898
 			}
 
 			return null;
